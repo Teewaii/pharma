7a729f236132b442d01054b661631970562a66d1
teewai <taiyeajimati@gmail.com>
2024-01-29 23:59:52 +0000

    missing logo fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import TopBar from "./utils/TopBar";
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo.png";
 import Link from "next/link";
 import HamBuger from "./HamBuger";
 import OverLay from "./OverLay";
