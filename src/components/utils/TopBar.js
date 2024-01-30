cf9f90df2229c0029954aaa183f7b4f23c507d57
teewai <taiyeajimati@gmail.com>
2024-01-30 02:36:55 +0000

    Make topBar collapsable

--- src/components/utils/TopBar.js
+++ src/components/utils/TopBar.js
@@ -1,13 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import phoneIcon from "../../assets/phone_1.svg";
 import emailIcon from "../../assets/thick@.svg";
+import { ChevronUpIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
 
 const TopBar = () => {
+  const [show, setShow] = useState(false);
+  const showAddress = () => {
+    setShow((prev) => !prev);
+  };
   return (
-    <div className="main bg-primary1 relative z-20">
+    <div
+      className={
+        show
+          ? "main bg-primary1 relative h-20 md:h-fit overflow-hidden z-20"
+          : "main bg-primary1 relative h-fit overflow-hidden z-20"
+      }
+    >
+      {/* <div className="main bg-primary1 relative  z-20"> */}
       <div className="wrapper text-white flex flex-col items-center container py-6  lg:flex-row lg:justify-between lg:items-center">
-        <p className="text-center max-md:mb-4 lg:text-left ">
+        <p className="text-center mb-4 lg:text-left ">
           Welcome to Pharma. We provides medical accessories
         </p>
         <div className="contactIcons flex flex-col items-center md:flex-row gap-2 lg:gap-6 ">
@@ -26,6 +41,19 @@ const TopBar = () => {
           </span>
         </div>
       </div>
+      {show ? (
+        <ChevronDownIcon
+          onClick={showAddress}
+          className="w-6 rounded-full bg-primary3 p-1 text-white absolute top-12 right-5 cursor-pointer transition-all ease-in-out duration-300
+          md:hidden z-70"
+        />
+      ) : (
+        <ChevronUpIcon
+          onClick={showAddress}
+          className="w-6 rounded-full bg-primary3 p-1 text-white absolute top-12 right-5 cursor-pointer transition-all ease-in-out duration-300
+          md:hidden z-70"
+        />
+      )}
     </div>
   );
 };
