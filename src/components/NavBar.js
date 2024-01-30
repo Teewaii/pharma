4d57e670dc5791e2c33e0a32d962d66f3bd90d76
teewai <taiyeajimati@gmail.com>
2024-01-30 01:38:36 +0000

    addition of shadow to the mobile menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,17 +28,17 @@ const NavBar = () => {
         {/* SMALL SCREEN NAVIGATION */}
         <div className=" container py-2 h-20 flex items-center justify-between relative sm:py-12 lg:hidden ">
           <Link href="/">
-            <Image src={logo} height={58} width={140} alt="logo" />
+            <Image src={logo} height={58} width={160} alt="logo" />
           </Link>
           <HamBuger toggle={toggle} Toggle={Toggle} />
           <div
             className={
               toggle
                 ? "flex flex-col gap-y-4  h-fit py-6 absolute left-0 right-0 -bottom-1 container -z-30 transition-all ease-in-out duration-300"
-                : "bg-white shadow-lg flex flex-col gap-y-4 h-fit py-6 absolute left-0 right-0 -bottom-13 container -z-30 transition-all ease-in-out duration-300"
+                : "bg-white shadow-xl flex flex-col gap-y-4 h-fit py-6 absolute left-0 right-0 -bottom-13 container -z-30 transition-all ease-in-out duration-300"
             }
           >
-            <ul className="flex flex-col gap-y-4  ">
+            <ul className="flex flex-col gap-y-4 ">
               {Object.entries(navLinks).map(([key, value]) => (
                 <li className="text-black" key={key}>
                   <Link className="text-lg " href={value}>
